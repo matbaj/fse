4cb8882e14469fc028717f17c6faff6180faa275
greyfox <greyfox@bshellz.pl>
2012-10-12 11:37:38 +0200

    Initial Commit (part 1)  What time is it? n________n |( •◡ • )|

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fse::Application.config.secret_token = '258ad5761e183cf973a31242b87b7fb1fc5982ed2bcf5f9c0c3dcbe5ab58f341b209edbc3d3a1380932ad366739883cd3c5a20a257946686a4749ee0c1bc365d'
